80ab719975161bfac1bf4439e3d3a8782e187c32
scubamut <scubamut@gmail.com>
2013-05-21 16:53:14 +0200

    update __init__.py

--- finlib/__init__.py
+++ finlib/__init__.py
@@ -4,4 +4,5 @@ from finlib.get_history_dict import get_history_dict
 from finlib.simulate import simulate
 from finlib.marketsim import marketsim
 from finlib.analyze import analyze
+from finlib.update_csv_data import update_csv_data
 import pandas as pd
