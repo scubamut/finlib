53da8c6721671786b0ee2fb1764571e4a3278b32
scubamut <scubamut@gmail.com>
2013-07-10 09:04:55 +0200

    new version no

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='finlib',
-    version='0.1.6',
+    version='0.1.7',
     author='Dave Gilbert',
     author_email='scubamut @gmail.com',
     packages=['finlib'],
