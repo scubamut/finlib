8a92f6adebe8049a49fc14193a0491993bb9eed9
scubamut <scubamut@gmail.com>
2013-07-10 09:03:14 +0200

    removed stats

--- finlib/simulate.py
+++ finlib/simulate.py
@@ -29,7 +29,7 @@ def simulate(start, end, symbols, weights, data_path) :
     df = pd.DataFrame(index=pdata.major_axis).ix[start:end]
 
     for i in range(len(symbols)):
-        ticker = symbols.ix[i]
+        ticker = symbols[i]
         allocation = weights[i]
         df[ticker] = pdata[ticker]['Adj Close']
         df[ticker + ' C'] = df[ticker] / df[ticker][0]
@@ -41,24 +41,17 @@ def simulate(start, end, symbols, weights, data_path) :
     df['Total'] =pd.DataFrame([df[ticker + ' I'] for ticker in symbols]).sum()
     df['Daily Rets'] = df.Total / df.Total.shift(1) - 1
 
-    Average_Daily_Return = df['Daily Rets'].mean()
-    Volatilty = df['Daily Rets'].std()
-    Sharpe_Ratio = sqrt(len(df)) * df['Daily Rets'].mean() / df['Daily Rets'].std()
-    Period_Return = df.Total[-1] / df.Total[0] - 1
-
-    return Average_Daily_Return, Volatilty, Sharpe_Ratio, Period_Return
+    return df[['Daily Rets', 'Total']]
 
 def main():
     pass
 
 if __name__ == '__main__':
 
-    import pandas as pd
     import datetime as dt
-    from finlib.simulate import simulate
 
-    data_path = 'G:\\Python Projects\\Computational Investing\\Data\\'
-    tickers = pd.read_csv(data_path + 'tickers.csv')
+    data_path = 'G:\\Google Drive\\Python Projects\\PyScripter Projects\\Computational Investing\\Data\\'
+##    tickers = pd.read_csv(data_path + 'tickers.csv')
 
     start_date = '1/1/2011'
     end_date = '31/12/2011'
@@ -66,17 +59,26 @@ if __name__ == '__main__':
     start = dt.datetime.strptime(start_date, '%d/%m/%Y').date()
     end = dt.datetime.strptime(end_date, '%d/%m/%Y').date()
 
-    symbols = tickers.ticker
-    weights = tickers.allocations
+    symbols = ['SPY', 'GLD']
+    weights = [0.8, 0.2]
 
-    Average_Daily_Return, Volatilty, Sharpe_Ratio, Period_Return = simulate(start, end, symbols, weights, data_path)
+    df = simulate(start, end, symbols, weights, data_path)
 
     print 'Symbols: ', symbols
     print 'Weights: ', weights
     print 'Start Date: ', start_date
     print 'End Date: ', end_date
     print 'Data Directory: ', data_path
+
+
+    Average_Daily_Return = df['Daily Rets'].mean()
+    Volatilty = df['Daily Rets'].std()
+    Sharpe_Ratio = sqrt(len(df)) * df['Daily Rets'].mean() / df['Daily Rets'].std()
+    Period_Return = df.Total[-1] / df.Total[0] - 1
+
     print 'Average Daily Return :', Average_Daily_Return
     print 'Volatilty: ', Volatilty
     print 'Sharpe Ratio: ', Sharpe_Ratio
     print 'Period Return: ', Period_Return
+
+
